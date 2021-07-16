f5634b4aec6b1e9549f5b87f87783538bdf35bce
Min Latt <minminlaxz@gmail.com>
2021-07-16 20:49:02 +0630

    Build docker image and push to the docker hub
    it is complicated for me to understand with docker local registery.

--- builddockerimage.sh
+++ builddockerimage.sh
@@ -2,3 +2,6 @@ docker build -t localhost:5001/minlaxz/node-kube-app:latest .
 
 docker push localhost:5001/minlaxz/node-kube-app:latest
 
+docker build -t minlaxz/node-kube-app:latest .
+
+docker push minlaxz/node-kube-app:latest

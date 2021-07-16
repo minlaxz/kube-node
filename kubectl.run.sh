d4cff1f23eac1a39008be5761337f35bab380539
Min Latt <minminlaxz@gmail.com>
2021-07-16 20:49:18 +0630

    K commands

---
+++ kubectl.run.sh
@@ -0,0 +1,14 @@
+k get nodes
+
+kubectl apply -f node-app-deployment.yaml
+
+k get pods
+k get deployments
+
+
+kubectl apply -f node-app-service.yaml
+
+k get services
+
+minikube service <service_name>
+
